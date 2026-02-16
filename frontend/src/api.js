12a9eadec0163dc13b6289b9a1ff3832394eb1a8
Sanchhiitt <smaheshwari@wattmonk.com>
2026-02-16 15:01:28 +0530

    chnaged the port

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'http://193.59.67.81:6000/api';
 
 export const generateChecks = async (rawText) => {
     const response = await axios.post(`${API_URL}/generate-checks`, {
